3d5f9dfcaa3b4f0714b4ad329f8b45d983bb80f4
teokuohong <kuohongteodev@gmail.com>
2022-10-16 17:22:42 +0800

    Completed frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, Container, Grid, makeStyles, Typography } from "@mui/material";
+import { Button, Card, Container, Grid, Typography } from "@mui/material";
 import io from "socket.io-client";
 import Big from 'big.js';
 
@@ -8,17 +8,26 @@ const socket = io("http://localhost:8888/");
 
 function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
-  const [isFetching, setIsFetching] = useState(false);
   const [piValue, setPiValue] = useState("0");
 
   const diameterOfTheSun = 1.3927 * 1000000;
 
+  const convertToDecimal = (value: string) => {
+    const insert = (arr: string[], index: number, newItem: string) => [
+      // part of the array before the specified index
+      ...arr.slice(0, index),
+      // inserted item
+      newItem,
+      // part of the array after the specified index
+      ...arr.slice(index)
+    ]
+    return insert(value.split(""), 1, ".").join("");
+
+  }
+
   const fetchPi = async () => {
-    setIsFetching(true);
     const value = await (await fetch("http://localhost:8888/getpi")).json();
-    setPiValue(value.value)
-    setIsFetching(false);
-    console.log(value);
+    setPiValue(convertToDecimal(value.value));
   }
 
   useEffect(() => {
@@ -33,7 +42,6 @@ function App() {
     return () => {
       socket.off("connect");
       socket.off("disconnect");
-      socket.off("pong");
     };
   }, []);
 
@@ -59,7 +67,7 @@ function App() {
           <Grid item>
             <Card sx={{ padding: "48px" }}>
               <Typography variant="h2">
-                Circumferrence Of The Sun
+                Circumferrence of The Sun
               </Typography>
               <Typography variant="body1" sx={{ wordBreak: "break-all", whiteSpace: "normal" }}>
                 {new Big(piValue).times(diameterOfTheSun).toString()} million kilometers
